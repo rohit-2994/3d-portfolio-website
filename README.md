efda0cf73afb584a334adc5f2c856a801f748293
ROHIT CHAUDHARY <chaudhryr2003@gmail.com>
2025-05-09 23:47:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 3d-portfolio-website
+ Personal Portfolio Website with ThreeJS and React.
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import styled, { ThemeProvider } from "styled-components";
+import {darkTheme} from "./utils/Themes";
+import Navbar from "./components/Navbar";
+import { BrowserRouter } from "react-router-dom";
+function App() {
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <BrowserRouter>
+        <Navbar/>
+      </BrowserRouter>
+
+    </ThemeProvider>
+  );
+}
+
+export default App;
